264d174457808c4de21c7d8579772836e62929ff
idriaxulus <cwwayne002@gmail.com>
2025-04-02 00:06:51 +0500

    0.1.0 -- Commit 1: up to the point where I can't figure out fucking sending the audio into the chat

---
+++ core/download.py
@@ -0,0 +1,28 @@
+from yt_dlp import YoutubeDL
+
+
+def get_video_title(url:str):
+    ydl_opts = {
+        'quiet': True,
+        'no_warnings': True,
+        'extract_flat': True,
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return info.get('title', 'Unknown Title')
+    
+
+def download_audio(url:str):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': 'downloads/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        return f"downloads/{info['title']}.mp3"
--- core/setup.py
+++ core/setup.py
@@ -2,9 +2,11 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import FSInputFile, InputMediaAudio
 from aiogram.filters import Command
 
 from .config import TOKEN
+from .download import get_video_title, download_audio
 
 
 logging.basicConfig(level=logging.INFO)
@@ -15,7 +17,28 @@ disp = Dispatcher()
 
 @disp.message(Command("start"))
 async def start_handler(message: types.Message):
-    await message.answer(f'Greetings! My purpose is to convert your Youtube video to an mp3 file. Please send the link to the video :)')
+    await message.answer(f'Greetings! My purpose is to convert your Youtube video to an mp3 file. Please send me the link to the video.')
+
+
+@disp.message()
+async def message_handler(message: types.Message):
+    if message.text.startswith('https://www.youtube.com/') or message.text.startswith('https://youtu.be/'):
+        title = get_video_title(message.text)
+
+        if title == 'Unknown Title':
+            await message.answer(f'No such video was found on Youtube. Try again, please.')
+            return 0
+
+        await message.answer(f'Found "{title}".')
+        
+        path = download_audio(message.text)
+
+
+        
+    else:
+        await message.answer(f'Please send me a valid Youtube link. {message.chat.id}')
+
+
 
 
 async def main():
